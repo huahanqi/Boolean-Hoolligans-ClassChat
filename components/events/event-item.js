102a167fa33f2d298758eec99527fc173c8e905c
Stella <stellazhu334@gmail.com>
2023-11-05 16:26:39 -0500

    add swipe to delete chat group

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,22 +1,24 @@
 import { useNavigation } from "@react-navigation/native";
-import { TouchableOpacity, StyleSheet, Text } from "react-native";
+import { TouchableOpacity, StyleSheet, Text, Animated, View } from "react-native";
 
 const EventItem = ({ id, title, description, wiki }) => {
   const navigation = useNavigation();
   return (
-    <TouchableOpacity
-      style={styles.card}
-      onPress={() =>
-        navigation.navigate("Chat", {
-          title,
-          description,
-          wiki,
-        })
-      }
-    >
-      <Text>{`${title}`}</Text>
-      <Text>{`${description}`}</Text>
-    </TouchableOpacity>
+
+    <Animated.View style={[styles.card]}>
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("Chat", {
+            title,
+            description,
+            wiki,
+          })
+        }
+      >
+        <Text style={styles.title}>{title}</Text> 
+        <Text style={styles.description}>{description}</Text>
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
@@ -28,6 +30,15 @@ const styles = StyleSheet.create({
     marginVertical: 5,
     marginHorizontal: 5,
     padding: 10,
+  },
+  title: { 
+    color: '#000', 
+    fontSize: 16, 
+    marginTop: -5,
+  },
+  description: {
+    color: 'gray',
+    fontSize: 14,
   },
 });
 
--- context/CourseContext.js
+++ context/CourseContext.js
@@ -12,18 +12,22 @@ export const CourseProvider = ({ children }) => {
         // Check if the course already exists based on id
         const courseExists = prevCourses.some(course => course.id === newCourse.id);
         if (!courseExists) {
-          const updatedCourses = [...prevCourses, newCourse];
-          updatedCourses.sort((a, b) => a.title.localeCompare(b.title));
-          Alert.alert('Success', 'Course Chat Added! 🎉', [{ text: 'OK' }]);
-          return updatedCourses;
+            const updatedCourses = [...prevCourses, newCourse];
+            updatedCourses.sort((a, b) => a.title.localeCompare(b.title));
+            Alert.alert('Success', 'Course Chat Added! 🎉', [{ text: 'OK' }]);
+            return updatedCourses;
         }
         Alert.alert('Sorry', 'Course Chat Added Before', [{ text: 'OK' }]);
         return prevCourses;
-      });
+    });
+  };
+
+  const deleteCourse = (courseToDelete) => {
+    setCourses(prevCourses => prevCourses.filter(course => course.id !== courseToDelete.id));
   };
 
   return (
-    <CourseContext.Provider value={{ courses, addCourse }}>
+    <CourseContext.Provider value={{ courses, addCourse, deleteCourse }}>
       {children}
     </CourseContext.Provider>
   );
