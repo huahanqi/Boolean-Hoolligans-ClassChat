4953a5a246a291df51a872f2faa5c84defa3ddc3
huahanqi <huahanqi@gmail.com>
2023-12-01 10:22:32 -0500

    fixed duplicate mocks issue

---
+++ frontend/App.js
@@ -0,0 +1,33 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import {
+  ActivityIndicator,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import "react-native-gesture-handler";
+import { AuthProvider } from "./context/AuthContext";
+import { CourseProvider } from "./context/CourseContext";
+import AppNav from "./navigation/appNav";
+
+export default function App() {
+  return (
+    <CourseProvider>
+      <AuthProvider>
+        <AppNav />
+        <StatusBar style="light" />
+      </AuthProvider>
+    </CourseProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
