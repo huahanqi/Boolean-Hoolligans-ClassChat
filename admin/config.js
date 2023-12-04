efe02409717452d2aeec713ce9e8aef2ba2a4816
huahanqi <huahanqi@gmail.com>
2023-12-04 11:09:29 -0500

    changed to remote api

--- admin/config.js
+++ admin/config.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = "http://localhost:4000/api";
-//export const API_ENDPOINT = "https://booleanhoolligans-8pravvog.b4a.run/api";
+//export const API_ENDPOINT = "http://localhost:4000/api";
+export const API_ENDPOINT = "https://booleanhoolligans-8pravvog.b4a.run/api";
